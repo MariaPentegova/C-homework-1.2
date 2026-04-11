c188b78e450745bdb8a991cb27ef458144bfe995
MariaPentegova <m.pentegova@yandex.ru>
2026-04-11 07:51:07 +0300

    Update nations.c

--- src/nations.c
+++ src/nations.c
@@ -1,143 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
-#define MAXN 10000
-#define MAXM 20000
-
-typedef struct Road {
-    int to;
-    int len;
-    struct Road* next;
-} Road;
-
-Road* graph[MAXN];
-
-void add_road(int u, int v, int len) {
-    Road* r1 = (Road*)malloc(sizeof(Road));
-    r1->to = v;
-    r1->len = len;
-    r1->next = graph[u];
-    graph[u] = r1;
-
-    Road* r2 = (Road*)malloc(sizeof(Road));
-    r2->to = u;
-    r2->len = len;
-    r2->next = graph[v];
-    graph[v] = r2;
-}
+#define INF 1e9
 
 typedef struct {
-    int dist;
-    int city;
-    int state;
-} Node;
-
-Node heap[MAXM];
-int heap_size = 0;
-
-void swap(Node* a, Node* b) {
-    Node tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void push(Node x) {
-    heap[++heap_size] = x;
-    int i = heap_size;
-    while (i > 1 && heap[i].dist < heap[i / 2].dist) {
-        swap(&heap[i], &heap[i / 2]);
-        i /= 2;
-    }
-}
-
-Node pop() {
-    Node res = heap[1];
-    heap[1] = heap[heap_size--];
-
-    int i = 1;
-    while (1) {
-        int left = 2 * i, right = 2 * i + 1, smallest = i;
-
-        if (left <= heap_size && heap[left].dist < heap[smallest].dist)
-            smallest = left;
-        if (right <= heap_size && heap[right].dist < heap[smallest].dist)
-            smallest = right;
-
-        if (smallest == i) break;
-
-        swap(&heap[i], &heap[smallest]);
-        i = smallest;
-    }
-
-    return res;
-}
+    int u, v, len;
+} Edge;
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
+    int n, m, k;
+    if (scanf("%d %d", &n, &m) != 2) return 1;
 
-    for (int i = 1; i <= n; i++)
-        graph[i] = NULL;
-
+    Edge *edges = malloc(m * sizeof(Edge));
     for (int i = 0; i < m; i++) {
-        int u, v, len;
-        scanf("%d %d %d", &u, &v, &len);
-        add_edge(u, v, len);
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].len);
     }
 
-    int k;
     scanf("%d", &k);
+    int *capitals = malloc(k * sizeof(int));
+    int *owner = malloc((n + 1) * sizeof(int)); // 0 - свободен, 1..k - номер гос-ва
 
-    int capitals[k];
-    for (int i = 0; i < k; i++)
-        scanf("%d", &capitals[i]);
+    for (int i = 1; i <= n; i++) owner[i] = 0;
 
-    int dist[MAXN];
-    int state[MAXN];
-
-    for (int i = 1; i <= n; i++) {
-        dist[i] = INT_MAX;
-        state[i] = -1;
-    }
-
-    // Инициализация столиц
     for (int i = 0; i < k; i++) {
-        int c = capitals[i];
-        dist[c] = 0;
-        state[c] = i;
-        push((Node){0, c, i});
+        scanf("%d", &capitals[i]);
+        owner[capitals[i]] = i + 1;
     }
 
-    // Мульти-Дейкстра
-    while (heap_size > 0) {
-        Node cur = pop();
+    int assigned = k;
+    while (assigned < n) {
+        // Проходим по очереди по каждому государству
+        for (int state = 1; state <= k; state++) {
+            if (assigned >= n) break;
 
-        if (cur.dist > dist[cur.city]) continue;
+            int min_dist = INF;
+            int best_city = -1;
 
-        Road* r = graph[cur.city];
-        while (r) {
-            int to = r->to;
-            int nd = cur.dist + r->len;
+            // Ищем ближайший город к текущему государству 'state'
+            for (int i = 0; i < m; i++) {
+                int u = edges[i].u;
+                int v = edges[i].v;
+                int len = edges[i].len;
 
-            if (nd < dist[to]) {
-                dist[to] = nd;
-                state[to] = cur.state;
-                push((Node){nd, to, cur.state});
+                // Если u в государстве, а v свободен
+                if (owner[u] == state && owner[v] == 0) {
+                    if (len < min_dist) { min_dist = len; best_city = v; }
+                }
+                // Если v в государстве, а u свободен
+                if (owner[v] == state && owner[u] == 0) {
+                    if (len < min_dist) { min_dist = len; best_city = u; }
+                }
             }
 
-            r = r->next;
+            // Если нашли город, забираем его
+            if (best_city != -1) {
+                owner[best_city] = state;
+                assigned++;
+            }
         }
     }
 
-    for (int i = 0; i < k; i++) {
-        printf("State %d:", i);
-        for (int j = 1; j <= n; j++) {
-            if (state[j] == i)
-                printf(" %d", j);
+    for (int state = 1; state <= k; state++) {
+        printf("Государство %d: ", state);
+        for (int i = 1; i <= n; i++) {
+            if (owner[i] == state) printf("%d ", i);
         }
         printf("\n");
     }
 
+    free(edges); free(capitals); free(owner);
     return 0;
 }
