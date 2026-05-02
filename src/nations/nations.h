f2e3e44aaca558ed969d205fad2a8c5c1cfcb4c6
MariaPentegova <m.pentegova@yandex.ru>
2026-05-02 14:53:14 +0300

    Update nations.h

--- src/nations/nations.h
+++ src/nations/nations.h
@@ -8,9 +8,5 @@ typedef struct {
 } Edge;
 
 void solve(int n, int m, Edge *edges, int k, int *capitals);
-static void assign_initial_capitals(int n, int k, int *capitals, int *owner);
-static void expand_territories(int n, int m, Edge *edges, int k, int *owner);
-static int find_closest_city(int owner_id, int m, Edge *edges, int *owner);
-static void print_results(int n, int k, int *owner);
 
 #endif // NATIONS_H
